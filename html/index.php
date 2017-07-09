0fc6c4e7d3b987d9d1a5f64d0fc89244c5b1be98
Jason Slavin [C] <jslavin@marketo.com>
2017-07-09 16:03:38 -0700

    Html structure in place

---
+++ html/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Redirecting to another file, to keep index.php clean
+ */
+
+header("Location:./src/url-preview.php");
+die();
--- html/src/url-preview.php
+++ html/src/url-preview.php
@@ -1,5 +1,20 @@
-
-<link rel="stylesheet" src="html/dist/styles/url-preview.css">
-<link rel="script" src="html/dist/styles/url-preview.js">
-
-echo '<h1>Some code will go here</h1>`;
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta lang="en">
+        <link rel="stylesheet" href="../dist/styles/url-preview.css">
+        <link rel="script" href="./scripts/url-preview.js">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <article>
+            <section>
+                <h1>URL Preview</h1>
+                <label for=".url-input">Tell us a site to checkout!</label>
+                <input class="url-input" type="text">
+                <div class="site-preview">
+                </div>
+            </section>
+        </article>
+    </body>
+</html>
