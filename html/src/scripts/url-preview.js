fa16f4f32b618fb92373e30d1e67ad45051de1f6
Jason Slavin [C] <jslavin@marketo.com>
2017-07-09 18:32:23 -0700

    Adds some styles for the site, as well as some JS for it

--- html/src/scripts/url-preview.js
+++ html/src/scripts/url-preview.js
@@ -1,3 +1,38 @@
-module.exports = function () {
-    console.log('hello');
-};
+$( document ).ready(function() {
+
+    console.log('ready');
+
+    let keypressTimeout;
+    console.log(keypressTimeout);
+
+    function typingCheck () {
+        if (keypressTimeout !== undefined ) {
+            clearTimeout(keypressTimeout);
+            keypressTimeout = setTimeout(searchForURL, 2000);
+        } else {
+            keypressTimeout = setTimeout(searchForURL, 2000);
+        }
+    }
+
+    function searchForURL () {
+        console.log('lets find that url!');
+    }
+
+    function checkInputValue (elem) {
+        let urlStart = 'www.';
+        let myValue = $(elem).val();
+
+        if (typeof myValue === 'string') {
+            console.log('this is a string');
+        }
+        ;
+        let myURL = myValue.match(/.www*/);
+        console.log(myURL);
+    }
+
+    $('.input-container input').on('keypress', (event)=> {
+        typingCheck();
+        checkInputValue(event.target);
+    });
+
+});
--- html/src/url-preview.php
+++ html/src/url-preview.php
@@ -3,15 +3,17 @@
         <meta charset="utf-8">
         <meta lang="en">
         <link rel="stylesheet" href="../dist/styles/url-preview.css">
-        <link rel="script" href="./scripts/url-preview.js">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script rel="script" src="./scripts/url-preview.js"></script>
     </head>
     <body>
         <article>
             <section>
                 <h1>URL Preview</h1>
-                <label for=".url-input">Tell us a site to checkout!</label>
-                <input class="url-input" type="text">
+                <div class="input-container">
+                    <label for="#url-input">Tell us a site to checkout!</label>
+                    <input title="url-input" id="url-input" type="text">
+                </div>
                 <div class="site-preview">
                 </div>
             </section>
